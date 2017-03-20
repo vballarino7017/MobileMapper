046d9cc13337ff739336fa227416121a06ffc115
student5 <student3@jh-mba158-01.local>
2017-03-20 10:00:49 -0500

    Initial Commit

---
+++ MobileMapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileMapper
+//
+//  Created by student3 on 3/20/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
