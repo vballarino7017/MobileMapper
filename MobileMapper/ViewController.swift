ce9e8c7d3a7d061f33a91f34390a3e72ef4bbad7
student5 <student3@jh-mba158-01.local>
2017-03-21 10:02:58 -0500

    added CLs and MKs found Eiffel drive

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let herseyAnnotation = MKPointAnnotation()
+    let address = "Eifel Tower"
+    let geocoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let longitude: Double = 42.102332924
+        let latitude: Double = -87.955661244
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        herseyAnnotation.coordinate = coordinate
+        herseyAnnotation.title = "John Hersey High School"
+        mapView.addAnnotation(herseyAnnotation)
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            for place in placemarks!
+            {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.mapView.addAnnotation(annotation)
+            }
+            
+        }
+        
     }
 
 
